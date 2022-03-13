c82b019073f4afccad40b4d88f43521442a6ea8b
贾舒淇 <jiashuqi@tiaozhan.com>
2022-03-13 12:54:36 +0800

    jsq complete record.js

--- src/api/reagent.js
+++ src/api/reagent.js
@@ -2,19 +2,6 @@ import { request } from "../utils/request";
 import { appInfo } from "../appInfo";
 
 async function addReagent(
-  name,
-  brand,
-  date,
-  purity,
-  capacity,
-  location,
-  type,
-  control,
-  solution,
-  operation,
-  limitation
-) {
-  return request(`${appInfo.baseurl}/api/reagent`, {
     name,
     brand,
     date,
@@ -26,16 +13,32 @@ async function addReagent(
     solution,
     operation,
     limitation
-  }, "POST");
+) {
+    return request(`${appInfo.baseurl}/api/reagent`, {
+        name,
+        brand,
+        date,
+        purity,
+        capacity,
+        location,
+        type,
+        control,
+        solution,
+        operation,
+        limitation
+    }, "POST");
 }
 
 async function deleteReagent(id) {
-  return request(`${appInfo.baseurl}/api/reagent/${id}`, undefined, "DELETE");
+    return request(`${appInfo.baseurl}/api/reagent/${id}`, undefined, "DELETE");
 }
+
+//记录模块
+
 
 
 
 export default {
-  addReagent,
-  deleteReagent
+    addReagent,
+    deleteReagent
 }
