931d924c2ec2866bca9a6822047efe4cdf3fd4f2
贾舒淇 <jiashuqi@tiaozhan.com>
2022-03-13 11:16:15 +0800

    jsq update some trash

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,23 @@
+<template>
+  <van-tabbar active="{{ active.value }}" :change="onChange">
+    <van-tabbar-item icon="home-o">标签</van-tabbar-item>
+    <van-tabbar-item icon="search" dot>标签</van-tabbar-item>
+    <van-tabbar-item icon="friends-o" info="5">标签</van-tabbar-item>
+    <van-tabbar-item icon="setting-o" info="20">标签</van-tabbar-item>
+  </van-tabbar>
+</template>
 <script>
 export default {
-  onLaunch: function () {
-    console.log('App Launch')
+  onLaunch: function() {
+    console.log("App Launch");
   },
-  onShow: function () {
-    console.log('App Show')
+  onShow: function() {
+    console.log("App Show");
   },
-  onHide: function () {
-    console.log('App Hide')
-  },
-}
+  onHide: function() {
+    console.log("App Hide");
+  }
+};
 </script>
 
 <style>
